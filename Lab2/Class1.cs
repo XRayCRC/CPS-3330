95db4f76fc29fcd224124fffc3607d7b90c1277b
XRayCRC <97985917+XRayCRC@users.noreply.github.com>
2023-02-10 10:33:09 -0500

    Add files via upload

---
+++ Lab2/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Task5
+{
+    /* Lab 2 Task 5
+     * Christian-Ray Conrad
+     * CPS 3330 - 03
+    public class Avg */
+    public class Avg
+    {
+        //Average of 2 Nums
+        public static double aver(double x, double y)
+        {
+            return (x+y) / 2;
+        }
+        //Average of 3 nums
+        public static double aver(double x, double y, double z)
+        {
+            return (x+y+ z) / 3;
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Task5;
+
+namespace TestAvg
+{
+    /* Lab 2 Task 5
+     * Christian-Ray Conrad
+     * CPS 3330 - 03
+    public class Avg */
+    public class TestAverage
+    {
+        public static void Main(string[] args)
+        {
+            //Get Number of Nums from user
+            Console.Write("How many numbers will you provide (2 or 3)? ");
+            double howMany = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a Value for 'a': ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a Value for 'b': ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            
+            if (howMany == 3)
+            {
+                //Get Third value if applicable
+                Console.Write("Enter a Value for 'c': ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                //Compute avg of 3 nums
+                Console.WriteLine("$Provided Numbers {0}, {1}, {2}. \nThe Average is {3}", a, b, c, 
+                   Task5.Avg.aver(a,b,c));
+            }
+            else
+            {
+                //Compute average of 2 nums
+                Console.WriteLine("$Provided Numbers {0}, {1}. \nThe Average is {2}", a, b,
+                    Task5.Avg.aver(a,b));
+            }
+        }
+    }
+}
